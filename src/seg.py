9a9e67e529b912a3b0799ba2bd036961c8b31cad
JT D <djt3019@mail.ustc.edu.cn>
2019-11-23 17:17:54 +0800

    seg

--- src/seg.py
+++ src/seg.py
@@ -1,18 +1,23 @@
 import jieba
 import pandas as pd
+import pickle
 import numpy as np
 
 
 def test_data():
     test_docs = pd.read_csv('../data/test_docs.csv')
     title_seg = []
+    test_docs[test_docs['doc_title'].isnull()] = '无'
     for target in test_docs['doc_title']:
         seg_list = jieba.lcut(target, cut_all=False)
-        print(seg_list)
         title_seg.append(' '.join(seg_list))
 
     print(title_seg)
+    with open('./title_seg.o', 'wb') as p:
+        pickle.dump(title_seg, p)
 
 
 if __name__ == '__main__':
-    test_data()
+    with open('./title_seg.o', 'rb') as r:
+        title_seg = pickle.load(r)
+    print(title_seg)
